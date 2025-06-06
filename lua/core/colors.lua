a663110b364974c45bc0afc3e2e683371134dbeb
Nenad Bursać <nbursa@gmail.com>
2025-06-06 14:33:11 +0200

    feat(colors): lighten default font color

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,10 +1,10 @@
 vim.cmd.colorscheme("kanagawabones")
 
 -- Set a gray background similar to the Codex UI
-vim.cmd([[highlight Normal guibg=#1e1e1e]])
-vim.cmd([[highlight NormalNC guibg=#1e1e1e]])
-vim.cmd([[highlight NormalFloat guibg=#1e1e1e]])
+vim.cmd([[highlight Normal guifg=#d0d0d0 guibg=#1e1e1e]])
+vim.cmd([[highlight NormalNC guifg=#d0d0d0 guibg=#1e1e1e]])
+vim.cmd([[highlight NormalFloat guifg=#d0d0d0 guibg=#1e1e1e]])
 -- Match nvim-tree background with the editor
-vim.cmd([[highlight NvimTreeNormal guibg=#1e1e1e]])
-vim.cmd([[highlight NvimTreeNormalNC guibg=#1e1e1e]])
-vim.cmd([[highlight NvimTreeNormalFloat guibg=#1e1e1e]])
+vim.cmd([[highlight NvimTreeNormal guifg=#d0d0d0 guibg=#1e1e1e]])
+vim.cmd([[highlight NvimTreeNormalNC guifg=#d0d0d0 guibg=#1e1e1e]])
+vim.cmd([[highlight NvimTreeNormalFloat guifg=#d0d0d0 guibg=#1e1e1e]])
