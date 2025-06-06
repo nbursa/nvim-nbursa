0e8ca587c7cd3dfa9f00e751d36df78dab4c4b89
Nenad Bursać <nbursa@gmail.com>
2025-06-06 14:22:22 +0200

    chore: set gray background

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,1 +1,6 @@
 vim.cmd.colorscheme("kanagawabones")
+
+-- Set a gray background similar to the Codex UI
+vim.cmd([[highlight Normal guibg=#1e1e1e]])
+vim.cmd([[highlight NormalNC guibg=#1e1e1e]])
+vim.cmd([[highlight NormalFloat guibg=#1e1e1e]])
