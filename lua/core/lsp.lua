00166a463d8724648ea9d05b7b493eb9dae5d955
Nenad Bursać <nbursa@gmail.com>
2025-06-06 14:03:45 +0200

    fix typescript server name

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -3,7 +3,7 @@ local lspconfig = require("lspconfig")
 lspconfig.gopls.setup({
 	cmd = { "gopls" },
 })
-lspconfig.ts_ls.setup({})
+lspconfig.tsserver.setup({})
 lspconfig.pyright.setup({})
 lspconfig.html.setup({})
 lspconfig.cssls.setup({})
