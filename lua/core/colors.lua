f0026f436ab197369779490319d9c5e63c4c1ae1
Nenad Bursać <nbursa@gmail.com>
2025-06-06 14:46:30 +0200

    feat(colors): grey out syntax groups

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,4 +1,5 @@
-vim.cmd.colorscheme("kanagawabones")
+-- Select a grayscale colorscheme
+vim.cmd.colorscheme("monochrome")
 
 -- Set a gray background similar to the Codex UI
 vim.cmd([[highlight Normal guifg=#d0d0d0 guibg=#1e1e1e]])
@@ -8,3 +9,19 @@ vim.cmd([[highlight NormalFloat guifg=#d0d0d0 guibg=#1e1e1e]])
 vim.cmd([[highlight NvimTreeNormal guifg=#d0d0d0 guibg=#1e1e1e]])
 vim.cmd([[highlight NvimTreeNormalNC guifg=#d0d0d0 guibg=#1e1e1e]])
 vim.cmd([[highlight NvimTreeNormalFloat guifg=#d0d0d0 guibg=#1e1e1e]])
+
+-- Make common syntax groups gray as well
+local gray_groups = {
+    "Comment",
+    "Constant",
+    "Identifier",
+    "Statement",
+    "PreProc",
+    "Type",
+    "Special",
+    "Directory",
+}
+
+for _, group in ipairs(gray_groups) do
+    vim.cmd(string.format("highlight %s guifg=#d0d0d0", group))
+end
